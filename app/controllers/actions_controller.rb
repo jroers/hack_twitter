85c3305067378a6df6a83b22d9771f9c70baea0d
jroers <joe.roers@gmail.com>
2017-05-06 13:45:41 -0700

    updated form and passing in handle to search for/create twitter users

---
+++ app/controllers/actions_controller.rb
@@ -0,0 +1,18 @@
+class ActionsController < ApplicationController
+  def search_twitter_user
+    handle = params[:twitter_user][:handle].gsub(' ', '')
+    handle = handle[0] == '@' ? handle : "@#{handle}"
+
+    @twitter_user = TwitterUser.where('handle = ?', handle).first
+    if @twitter_user.blank?
+      @message = "Entered new twitter user"
+      @twitter_user = TwitterUser.create(:handle => handle)
+    else
+      @message = "Found existing twitter user"
+    end
+
+    respond_to do |format|
+      format.js { render 'twitter_user_response' }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+
+  resources :actions do
+    collection do
+      get 'search_twitter_user'
+    end
+  end
 end
